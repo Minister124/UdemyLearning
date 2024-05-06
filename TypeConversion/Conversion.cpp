6bdf9713297633bb332e99adc9ce5ee638c07221
Minister124 <kushaltmg321@gmail.com>
2024-05-06 09:15:51 +0545

    Fourth Commit

---
+++ TypeConversion/Conversion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+
+//----------------------------------------------------------------
+
+int value = 10;                 //implicit Conversion where compiler automatically compiles when it feels safe to do so.
+double y = value;
+
+//----------------------------------------------------------------
+
+//----------------------------------------------------------------
+
+double z = 5.67;
+int x = (int)z;             //Explicit Conversion (C-Style Casting)
+
+int xx = 5.4;
+double xy = (double)xx;   //Explicit Conversion (C-Style Casting))
+
+//----------------------------------------------------------------
+
+//----------------------------------------------------------------
+
+
+
